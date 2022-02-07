73cdebd0de1459f81c6a189425cffce33e1603e5
VitaliiBashta <bashta@email.cz>
2022-02-07 21:07:44 +0100

    formatting

--- src/main/java/com/l2jserver/gameserver/model/clientstrings/BuilderText.java
+++ src/main/java/com/l2jserver/gameserver/model/clientstrings/BuilderText.java
@@ -1,31 +1,10 @@
-/*
- * Copyright © 2004-2021 L2J Server
- * 
- * This file is part of L2J Server.
- * 
- * L2J Server is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- * 
- * L2J Server is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- * General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License
- * along with this program. If not, see <http://www.gnu.org/licenses/>.
- */
 package com.l2jserver.gameserver.model.clientstrings;
 
-/**
- * @author Forsaiken
- */
 final class BuilderText extends Builder {
-	private final String _text;
-	
+  private final String text;
+
 	BuilderText(final String text) {
-		_text = text;
+    this.text = text;
 	}
 	
 	@Override
@@ -45,6 +24,6 @@ final class BuilderText extends Builder {
 	
 	@Override
 	public String toString() {
-		return _text;
+    return text;
 	}
 }
